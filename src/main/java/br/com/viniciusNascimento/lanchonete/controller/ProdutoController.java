d31473a7158d5b9f8b4db579da7933e6710c39b6
Vinicius Nascimento <nascimento.13.v@gmail.com>
2023-09-12 15:06:15 -0300

    adicionando o atualizar parcial na classe produto

--- src/main/java/br/com/viniciusNascimento/lanchonete/controller/ProdutoController.java
+++ src/main/java/br/com/viniciusNascimento/lanchonete/controller/ProdutoController.java
@@ -4,13 +4,17 @@ import br.com.viniciusNascimento.lanchonete.exception.ProdutoNaoEncontradoExcept
 import br.com.viniciusNascimento.lanchonete.model.Produto;
 import br.com.viniciusNascimento.lanchonete.repository.ProdutoRepository;
 import br.com.viniciusNascimento.lanchonete.service.CadastroProdutoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -44,10 +48,10 @@ public class ProdutoController {
     @PutMapping("/{produtoId}")
     public ResponseEntity<Produto>
     atualizar(@PathVariable Long produtoId,
-              @RequestBody Produto produto) {
+              @RequestBody Optional<Produto> produto){
         Optional<Produto> produtoAtual
                 = produtoRepository.findById(produtoId);
-        if (produtoAtual.isPresent()) {
+        if(produtoAtual.isPresent()){
             BeanUtils.copyProperties
                     (produto, produtoAtual.get(), "id");
             Produto produtoSalvo
@@ -55,5 +59,34 @@ public class ProdutoController {
             return ResponseEntity.ok(produtoSalvo);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping("/{produtoId}")
+    public ResponseEntity<Produto> atualizarParcial
+            (@PathVariable Long produtoId,
+             @RequestBody Map<String, Object> campos) {
+        Optional<Produto> produtoAtual
+                = produtoRepository.findById(produtoId);
+        if (produtoAtual == null) {
+            return ResponseEntity.notFound().build();
+        }
+        merge(campos,  produtoAtual);
+        return atualizar(produtoId, produtoAtual);
+    }
+    private void merge(Map<String, Object> dadosOrigem,
+                       Optional<Produto> produtoDestino) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Produto produtoOrigem =
+                objectMapper.convertValue(dadosOrigem,
+                        Produto.class);
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade)
+                -> {Field field = ReflectionUtils.findField(
+                Produto.class, nomePropriedade);
+            field.setAccessible(true);
+            Object novoValor =
+                    ReflectionUtils.getField(field, produtoOrigem);
+            ReflectionUtils.setField(field,
+                    produtoDestino, novoValor);
+        });
     }
 }
